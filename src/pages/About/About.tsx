fc7989091f955812833c0b62df906b698d7a3ff2
unknown <ismayiloff522@gmail.com>
2024-08-25 01:21:16 +0400

    about page

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
 
-const About = () => {
+const About: React.FC = () => {
   return (
-    <div>About</div>
+    <div className='about-page'>
+        <div className="hero d-flex flex-column align-items-center justify-content-center w-100">
+          <p className='text-uppercase text-light fw-bold'>ninetheme company</p>
+          <h3 className='text-light fw-bold text-center mt-3'>We believe we can all <br /> make a difference.</h3>
+          <h5 className='text-light text-center mt-3'>Aesthetic brunch polaroid bespoke, vice kale <br /> chips leggings 90’s selfies raw denim.</h5>
+        </div>
+      <div className="containers">
+      </div>
+    </div>
   )
 }
 
